f88c31814afa623ccfa70942f6fcbc652153c236
Anthony <rappan@oregonstate.edu>
2024-01-14 21:04:01 -0800

    changed int input to float input

--- average.py
+++ average.py
@@ -3,11 +3,12 @@
 # 1/14/24
 # Python Project 2a: Average of 5 input numbers
 
-
-num_1 = float(input("Please enter five numbers: "))
+print("Please enter five numbers.")
+num_1 = float(input())
 num_2 = float(input())
 num_3 = float(input())
 num_4 = float(input())
 num_5 = float(input())
 sum = num_1 + num_2 + num_3 + num_4 + num_5
-print("the average of those numbers is" , float(sum / 5))
+print("the average of those numbers is:")
+print(float(sum / 5))
