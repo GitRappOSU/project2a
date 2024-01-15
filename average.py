139aeb5715812a3b95fb9cadec253cfd1eb4d003
Anthony <rappan@oregonstate.edu>
2024-01-14 20:44:41 -0800

    average name updated

---
+++ average.py
@@ -0,0 +1,12 @@
+# Anthony Rapp
+# GitHUb: GitRappOSU
+# 1/14/24
+# Python Project 2a: Average of 5 input numbers
+num_1 = int(input("Please Enter 5 numbers, Separated by Enter: "))
+num_2 = int(input())
+num_3 = int(input())
+num_4 = int(input())
+num_5 = int(input())
+sum = num_1 + num_2 + num_3 + num_4 + num_5
+print("the average of those numbers is")
+print(int(sum / 5))
